60ab3cff6942762ca32d864cf51f199e43da6297
tuanhm <hmtuan9a@gmail.com>
2024-10-04 16:56:29 +0700

    added hello controller

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
